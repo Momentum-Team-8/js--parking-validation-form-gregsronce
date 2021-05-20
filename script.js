518cd9c1f78e69cecab89fbb5a044660be2ee781
Greg Sronce <greg.sronce@gmail.com>
2021-05-20 17:04:37 -0400

    Completed step 3 in HTML, will try to do in JS.

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log('Add validation!');
+
+// CVV must be a three-digit number. 
+// This can use the pattern attribute, or custom validation with JavaScript.
+// Done in HTML, will try with JavaScript later
