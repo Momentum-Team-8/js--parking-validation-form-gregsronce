a7f6b5db2dfa9cccfa612445142a626a7a66a669
Greg Sronce <greg.sronce@gmail.com>
2021-05-21 17:04:04 -0400

    step 4 completed, thanks, Jeanette, back to steps 1-3.

--- script.js
+++ script.js
@@ -4,39 +4,85 @@ console.log('Add validation!');
 // This can use the pattern attribute, or custom validation with JavaScript.
 // Done in HTML, will try with JavaScript later
 
-// Add the ability to show the user the total cost of their parking when they click the "Make Reservation" button. 
-// The div with id "total" should be filled with text showing the cost. 
-// This should only occur if the form is valid.
-// The cost is $5 per day.
+// 1. Add the ability to show the user the total cost of their parking when they click the "Make Reservation" button.
+// 1a. Make a variable of #submit-button.
 
-const makeReservation = document.querySelector('#submit-button')
-const numberOfDays = document.querySelector('#days-field')
-const total = document.querySelector('#total')
+
+
+
+// Variables 
+let nameInput = document.querySelector('#name')
+let numberOfDays = document.querySelector('#days')
+let submitButton = document.querySelector('#submit-button')
+let total = document.querySelector('#total')
+let error = document.createElement("div")
 let form = document.querySelector('#parking-form')
-let formIsValid;
-
-form.addEventListener('input', event => {
-    console.log('input captured again!')
-})
 
 form.addEventListener('submit', event => {
-    event.preventDefault
-    if (numberofDays.value === 0) {
-    } formIsValid = false
+  event.preventDefault()
+  validateDays()
 })
 
-makeReservation.addEventListener('click', event => {
-     console.log('input captured!')
-
-     total.innerHTML = event.target.numberOfDays
+// 1d. Make addEventListener using the 'click' event to #submit-button.
+submitButton.addEventListener('click', event => {
+    console.log('input captured!')
+    console.log(event)  
 })
 
+function validateDays() {
+  if (numberOfDays.value === "") {
+    console.log('no input')
+    console.log('inside validateDays')
+  document.querySelector('#days-field').classList.add('input-invalid')
+  document.querySelector('#days-field').appendChild(error).innerHTML = 'This field needs numbers, you dolt.'
+  } else if (numberOfDays.value <= 1 || numberOfDays.value >= 30) {
+    document.querySelector('#days-field').classList.add('input-invalid')
+    document.querySelector('#days-field').appendChild(error).innerHTML = 'This field requires numbers between 1-30.'
+  } else {
+    document.querySelector('#days-field').classList.add('input-valid')
+    total.innerText = '$' + numberOfDays.value * 5
+  }
+}
 
- numberOfDays.addEventListener('input', event => {
-   
-    
-})
-//total.classList.add('input-field')
-//total.appendChild.innerHTML
+
+// 2. The div with id "total" should be filled with text showing the cost. 
 
 
+// Make a variable of #total.
+// let total = document.querySelector('#total')
+ 
+
+
+// 3. This should only occur if the form is valid.
+
+
+let formIsValid;
+function validateInput() {
+if (numberOfDays.value === undefined) {
+  formIsValid = false
+  
+ }
+}
+
+
+
+
+
+
+
+// document.getElementById(#totalcost).innerHTMl = $ + total
+// let total = numberOfDays * 5
+
+//function validate ()
+// all the things you need to validate
+
+
+
+
+// step 5 use arrays and map to get the totals
+// Look at map method with arrays
+// .map is a function within a function (callback function)
+//newPets = pets.map(function(pet){
+// return _.capitalize(pet)
+// })
+// only use return with curly braces, if the function is on multiple lines, or if you're not using an arrow function.
